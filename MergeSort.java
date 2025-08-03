d8e8179b42b2d2a5b76f7291361c1998aaf1f494
Pranesh <praneshmadhan646@gmail.com>
2025-08-03 20:26:00 +0530

    Fixed Typo's

--- MergeSort.java
+++ MergeSort.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
 public class MergeSort{
     public static void main(String[] js){
         int[] a = {5,2,6,4,1,3};
-        mergerSort(a);
+        mergeSort(a);
+        System.out.print(Arrays.toString(a));
     }
-    public static void mergerSort(int[] a){
+    public static void mergeSort(int[] a){
         int l = 0,r=a.length-1;
         mergerSort(a,l,r);
     }
@@ -19,9 +21,9 @@ public class MergeSort{
         int n1 = m-l+1,n2 = r-m;
         int[] L = new int[n1],R=new int[n2];
         for(int i=0;i<n1;i++) L[i] = a[l+i];
-        for(int i=0;i<n2;i++) R[i] = a[r+i+1];
+        for(int i=0;i<n2;i++) R[i] = a[m+i+1];
         int i=0,j=0,k=l;
-        while(i<n1 && j<n2) a[k++] = L[i]<=R[i] ? L[i++] : R[j++];
+        while(i<n1 && j<n2) a[k++] = L[i]<=R[j] ? L[i++] : R[j++];
         while(i<n1) a[k++] = L[i++];
         while(j<n2) a[k++] = R[j++];
     }
